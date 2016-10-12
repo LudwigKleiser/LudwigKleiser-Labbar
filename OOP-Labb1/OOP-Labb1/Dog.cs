3f1c3881bef2bd5205cf8a57ebfd812b89a1ece2
ludwig <ludwig>
2016-10-12 11:38:25 +0200

    Lagt upp på github

---
+++ OOP-Labb1/OOP-Labb1/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Labb1
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+
+        public Dog(string name, int age, string breed )
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+        }
+    }
+}
---
+++ OOP-Labb1/OOP-Labb1/RunTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Labb1
+{
+    class RunTime
+    {
+        public void Start()
+        {
+            Dog d1 = new Dog("Ludde", 5, "Bulldog");
+            Dog d2 = new Dog("Dutten", 2, "Tax");
+            Dog d3 = new Dog("Plutten", 7, "Pug");
+
+            Console.WriteLine(d1.Name + " " + d1.Age+ " " + d1.Breed);
+            Console.WriteLine(d2.Name + " " + d2.Age + " " + d2.Breed);
+            Console.WriteLine(d3.Name + " " + d3.Age + " " + d3.Breed);
+
+        }
+    }
+}
